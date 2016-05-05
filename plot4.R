e373690fd4db6d55c684bcd70e278c181c5837f7
julien <julienlafontaine2006@gmail.com>
2016-05-05 22:02:02 +1000

    Added plots for questions 4 and 5

---
+++ plot4.R
@@ -0,0 +1,26 @@
+# Load the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Select coal combustion source only
+isCombustion <- grepl(SCC$Short.Name, pattern = "comb", ignore.case = TRUE)
+isCoal <- grepl(SCC$Short.Name, pattern = "coal", ignore.case = TRUE)
+
+isCoalCombustion <- (isCombustion & isCoal)
+
+coalCombustionSource <- SCC[isCoalCombustion,]$SCC
+
+NEI <- subset(NEI, SCC %in% coalCombustionSource)
+
+# Calculate total pm 2.5 emission per year
+pm25 <- tapply(NEI$Emissions, NEI$year, sum)
+
+# Plot the chart
+opt <- options(scipen = 10)
+
+plot(names(pm25), pm25, xlab = "Year", ylab = "PM2.5 (tons)", main = "PM2.5 emission from coal combustion sources in the US")
+lines(names(pm25), pm25)
+
+# Generate png file
+dev.copy(device = png, filename = "plot4.png", width = 480, height = 480)
+dev.off()
---
+++ plot5.R
@@ -0,0 +1,24 @@
+# Load the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Select vehicle combustion source only
+isVehicle <- grepl(SCC$SCC.Level.Two, pattern = "vehicle", ignore.case = TRUE)
+vehicleSource <- SCC[isVehicle,]$SCC
+NEI <- subset(NEI, SCC %in% vehicleSource)
+
+# Select Baltimore City emission only
+NEI <- subset(NEI, fips == 24510)
+
+# Calculate total pm 2.5 emission per year
+pm25 <- tapply(NEI$Emissions, NEI$year, sum)
+
+# Plot the chart
+opt <- options(scipen = 10)
+
+plot(names(pm25), pm25, xlab = "Year", ylab = "PM2.5 (tons)", main = "PM2.5 emission from vehicle sources in Baltimore City")
+lines(names(pm25), pm25)
+
+# Generate png file
+dev.copy(device = png, filename = "plot5.png", width = 480, height = 480)
+dev.off()
